1a4c4295c8b8bf899a7065ac53f227d6c0e7ffda
John Siwicki <johnsiwicki@gmail.com>
2019-02-04 08:31:09 -0500

    Create ll-bg.js

---
+++ ll-bg.js
@@ -0,0 +1,34 @@
+function lazyBG(element) {	
+	//store out Bgs
+	var lazyBackgrounds = [].slice.call(document.querySelectorAll(element));
+
+	if (
+		"IntersectionObserver" in window &&
+		"IntersectionObserverEntry" in window &&
+		"intersectionRatio" in window.IntersectionObserverEntry.prototype
+	) {
+		//add a background of none
+		for (i = 0; i < lazyBackgrounds.length; i++) {
+			lazyBackgrounds[i].style.background = "none";
+		}
+
+		//check out whats in our scope
+		let lazyBackgroundObserver = new IntersectionObserver(function(
+			entries,
+			observer
+		) {
+			entries.forEach(function(entry) {
+				if (entry.isIntersecting) {
+					entry.target.removeAttribute("style");
+					lazyBackgroundObserver.unobserve(entry.target);
+				}
+			});
+		});
+
+		lazyBackgrounds.forEach(function(lazyBackground) {
+			lazyBackgroundObserver.observe(lazyBackground);
+		});
+	}
+}
+
+export { lazyBG };
